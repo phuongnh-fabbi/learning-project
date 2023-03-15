2a30099ccf4ddd0287123faa267302f1c2e95ee1
phuongnh-fabbi <phuongnh@fabbi.com.vn>
2023-03-15 10:05:06 +0700

    Fix comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users do
-    get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
   end
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
