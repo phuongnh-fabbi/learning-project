0ec896ae975d0abde1dd42bc36a504381702a13c
phuongnh-fabbi <phuongnh@fabbi.com.vn>
2023-04-07 17:31:01 +0700

    feat: manage lesson

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,9 @@
 class Question < ApplicationRecord
   belongs_to :lessons
-  has_many :answers
+  has_many :answers, dependent: :destroy, inverse_of: :question, autosave: true
+
+  accepts_nested_attributes_for :answers, allow_destroy: true,
+    reject_if: proc {|attributes| attributes[:content].blank?}
+
+  self.primary_key = "id"
 end
